6111944ab0b9795a6a3780a777d8c12f2d795b92
Jeremy Lee <jerlee@umass.edu>
2017-04-14 12:50:26 -0400

    static

--- server/src/server.js
+++ server/src/server.js
@@ -1,29 +1,18 @@
-var reverseString = require('./util.js');
 var bodyParser = require('body-parser');
 
-// Imports the express Node module.
+
 var express = require('express');
-// Creates an Express server.
+
 var app = express();
 
+// You run the server from `server`, so `../client/build` is `server/../client/build`.
+// '..' means "go up one directory", so this translates into `client/build`!
+app.use(express.static('../client/build'));
+
 app.use(bodyParser.text());
-// Defines what happens when it receives the `GET /` request
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
 
-// Starts the server on port 3000!
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
-});
 
-app.post('/reverse', function(req, res){
-  if (typeof(req.body) === 'string'){
-    var reversed = reverseString(req.body);
-    res.send(reversed);
-  }
 
-  else{
-    res.status(400).end()
-  }
+app.listen(3000, function(){
+  console.log('Example app listening on port 3000');
 });
